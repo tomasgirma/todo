ccebfde1ea4aab428ea550ba578b95a9cc9c8a45
Tomas <thomasgirma159@gmail.com>
2024-12-18 12:17:10 +0300

    adding time

--- fun.py
+++ fun.py
@@ -1,7 +1,15 @@
-def check_state(value):
-    if value >= 100:
-        return 'Gas'
-    elif 100 > value >= 0:
-        return 'Liquid'
-    else:
-        return 'Solid'
+def get_todos(file_path):
+    with open(file_path, "r") as local_file:
+        local_todos = local_file.readlines()
+    return local_todos
+
+
+def save_todos(todos, file_path):
+    with open(file_path, "w") as local_file:
+        local_file.writelines(todos)
+
+
+def command():
+    commands = input("Enter add, show, edit, complete or exit: ")
+    commands.strip()
+    return commands
